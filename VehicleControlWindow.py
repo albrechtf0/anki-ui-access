fd0fc3b53b8047644fe86be9940e4e99c296438f
albrechtf0 <fel.albrecht@heinrichheinedo.de>
2023-03-30 12:04:25 +0200

    Implement lane chaneg Speed

--- VehicleControlWindow.py
+++ VehicleControlWindow.py
@@ -15,12 +15,12 @@ class vehicleControler:
         for i in range(len(vehicleSelection)):
             if(vehicleSelection[i].get() == 1):
                 self.eventLoop.create_task(self.vehicles[i].stop())
-    def changeLane(self,vehicleSelection: list[IntVar],lane:StringVar):
+    def changeLane(self,vehicleSelection: list[IntVar],lane:StringVar,laneSpeed: IntVar):
         ln = next(filter(lambda x: x.lane_name == lane.get(),self.lanes))
         for i in range(len(vehicleSelection)):
             print("cVeh")
             if(vehicleSelection[i].get() == 1):
-                task = self.eventLoop.create_task(self.vehicles[i].changeLane(ln,100))
+                task = self.eventLoop.create_task(self.vehicles[i].changeLane(ln,laneSpeed.get()))
                 task.add_done_callback(lambda t: print(t,"is done!"))
                 print(f"ChangingLane to{ln}")
     
@@ -34,13 +34,14 @@ class vehicleControler:
         frm = ttk.Frame(root, padding=10)
         frm.grid()
         ttk.Entry(frm,)
-        speedValue = IntVar()
+        speedValue = IntVar(frm)
+        laneSpeed = IntVar(frm)
         
-        ttk.Button(frm, text="exit",command=self.stop).grid(column=0, row=0)
-        ttk.Button(frm, text="Start",command=lambda: self.startVehicle(speedValue,vSStates),).grid(column=0, row=1)
+        ttk.Button(frm, text="Exit",command=self.stop).grid(column=0, row=0)
+        ttk.Button(frm, text="Set Speed",command=lambda: self.startVehicle(speedValue,vSStates),).grid(column=0, row=1)
         ttk.Button(frm, text="Stop",command=lambda: self.stopVehicle(vSStates),).grid(column=0, row=2)
         Scale(frm,from_=1000,to=100,variable=speedValue,orient="vertical",tickinterval=10).grid(column=1,row=1,rowspan=4)
-        
+        Scale(frm,from_=100,to=400,variable= laneSpeed,orient="horizontal").grid(column=0,row=5,columnspan=2)
         
         vSfrm = ttk.Frame(frm,padding=10,border=1)
         vSfrm.grid(column=2,row=0,rowspan=len(vehicles))
