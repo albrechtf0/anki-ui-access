95ab5d44fecbffbd34207d681023e00b4d2f40b7
albrechtf0 <fel.albrecht@heinrichheinedo.de>
2024-01-08 11:53:52 +0100

    Updating VehicleControlWindow to support new line system

--- VehicleControlWindow.py
+++ VehicleControlWindow.py
@@ -1,5 +1,5 @@
 import anki
-from anki.utility.lanes import _Lane
+from anki.misc.lanes import BaseLane
 import asyncio
 from tkinter import *
 from tkinter import ttk as ttk
@@ -20,13 +20,18 @@ class vehicleControler:
         ln = next(filter(lambda x: x.lane_name == lane.get(),self.lanes))
         for i in range(len(vehicleSelection)):
             if(vehicleSelection[i].get() == 1):
-                asyncio.run_coroutine_threadsafe(self.vehicles[i].changeLane(ln,laneSpeed.get()),
+                asyncio.run_coroutine_threadsafe(self.vehicles[i].change_lane(ln,laneSpeed.get()),
                                                  self.eventLoop)
     
-    def __init__(self,vehicles:list[anki.Vehicle], eventLoop:asyncio.AbstractEventLoop,customLanes:list[_Lane]=[]) -> None:
+    def __init__(
+        self,
+        vehicles:list[anki.Vehicle], 
+        eventLoop:asyncio.AbstractEventLoop,
+        lanes:list[BaseLane]=[]
+    ) -> None:
         self.vehicles = vehicles
         self.eventLoop:asyncio.AbstractEventLoop = eventLoop
-        self.lanes = anki.Lane3.getAll()+anki.Lane4.getAll()+customLanes
+        self.lanes = lanes
         root = Tk()
         root.title("Vehicle Control")
         
@@ -52,12 +57,12 @@ class vehicleControler:
         ttk.Label(vSfrm,text="Vehicles: ").grid(column=0,row=0)
         for i in range(len(vehicles)):
             vSStates.append(IntVar())
-            btn = ttk.Checkbutton(vSfrm,text=f"Vehicle {i}",variable=vSStates[i],state=0).grid(column=0,row=i+1)
+            btn = ttk.Checkbutton(vSfrm,text=f"Vehicle {i}",variable=vSStates[i],state="0").grid(column=0,row=i+1)
         lsLane = StringVar(frm)
         #lane changes
         lcFrm = ttk.Frame(frm,padding=10,borderwidth=5,relief="groove")
         lcFrm.grid(column=0,row=1,)
-        ttk.OptionMenu(lcFrm,lsLane,self.lanes[0],*self.lanes).grid(column=0,row=0)
+        ttk.OptionMenu(lcFrm,lsLane,str(self.lanes[0]),*(str(lane) for lane in self.lanes)).grid(column=0,row=0)
         ttk.Button(lcFrm,text="Change Lane",command=lambda:self.changeLane(vSStates,lsLane,laneSpeed)).grid(column=0, row=1 )
         ttk.Label(lcFrm,text="Vehicles do not\nchange lanes when\nslower than 300 speed").grid(column=0,row=2)
         #lane change speed
