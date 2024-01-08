43f4c7eea690f80d1d935bf064f70cb4f836b9d8
albrechtf0 <fel.albrecht@heinrichheinedo.de>
2024-01-08 12:04:23 +0100

    Updating main.py to new sdk version

--- __main__.py
+++ __main__.py
@@ -1,13 +1,12 @@
 import anki
-from anki.utility.lanes import _Lane
 import asyncio
 import random
+import pygame
 try:
     from .UiMain import Ui
     from .Design import Design
 except ImportError:
     from UiMain import Ui
-    import pygame
     from Design import Design
 
 
@@ -15,15 +14,15 @@ async def main():
     print("Start")
     pygame.init()
     async with anki.Controller() as control:
-        autos = await control.connectMany(1)
-        #await control.scan()
-        autos[0]._position = 0
-        with Ui(autos,fakeMap(),(1,0),False) as Uiob:
+        autos = await control.connect_many(1)
+        #autos[0]._position = 0
+        await control.scan() # type: ignore
+        with Ui(list(autos),control.map,(1,0),False) as Uiob:
             Uiob.addEvent("test")
             while True:
                 await asyncio.sleep(10)
-                des = Design(Background=(45, 167, 184), Line=(169, 77, 95), LineWidth=0, Text=(99, 121, 67), CarPosText=(61, 88, 241), ButtonFill=(40, 237, 105, 236), EventFill=(225, 109, 137), CarInfoFill=(144, 12, 47), ConsoleHeight=303, ShowGrid=False, ShowOutlines=True)
-                Uiob.setDesign(des)
+                #des = randDes()
+                #Uiob.setDesign(des)
                 #print(des)
 
 def randColor():
@@ -33,7 +32,7 @@ def randColorA():
 def randDes():
     return Design(randColor(),
                              randColor(),
-                             random.randint(0,5),
+                             random.randint(1,5),
                              randColor(),
                              randColor(),
                              randColorA(),
@@ -44,23 +43,23 @@ def randDes():
                              random.randint(0,1)==1)
 def fakeMap():
     map = [
-        anki.TrackPiece(0,anki.TrackPieceTypes.START,False),
-        anki.TrackPiece(0,anki.TrackPieceTypes.CURVE,False),
-        anki.TrackPiece(0,anki.TrackPieceTypes.CURVE,False),
-        anki.TrackPiece(0,anki.TrackPieceTypes.CURVE,False),
-        anki.TrackPiece(0,anki.TrackPieceTypes.STRAIGHT,False),
-        anki.TrackPiece(0,anki.TrackPieceTypes.CURVE,True),
-        anki.TrackPiece(0,anki.TrackPieceTypes.CURVE,True),
-        anki.TrackPiece(0,anki.TrackPieceTypes.CURVE,True),
-        anki.TrackPiece(0,anki.TrackPieceTypes.CURVE,True),
-        anki.TrackPiece(0,anki.TrackPieceTypes.CURVE,True),
-        anki.TrackPiece(0,anki.TrackPieceTypes.CURVE,True),
-        anki.TrackPiece(0,anki.TrackPieceTypes.STRAIGHT,True),
-        anki.TrackPiece(0,anki.TrackPieceTypes.CURVE,False),
-        anki.TrackPiece(0,anki.TrackPieceTypes.CURVE,False),
-        anki.TrackPiece(0,anki.TrackPieceTypes.CURVE,False),
-        anki.TrackPiece(0,anki.TrackPieceTypes.STRAIGHT,False),
-        anki.TrackPiece(0,anki.TrackPieceTypes.FINISH,False)]
+        anki.TrackPiece(0,anki.TrackPieceType.START,False),
+        anki.TrackPiece(0,anki.TrackPieceType.CURVE,False),
+        anki.TrackPiece(0,anki.TrackPieceType.CURVE,False),
+        anki.TrackPiece(0,anki.TrackPieceType.CURVE,False),
+        anki.TrackPiece(0,anki.TrackPieceType.STRAIGHT,False),
+        anki.TrackPiece(0,anki.TrackPieceType.CURVE,True),
+        anki.TrackPiece(0,anki.TrackPieceType.CURVE,True),
+        anki.TrackPiece(0,anki.TrackPieceType.CURVE,True),
+        anki.TrackPiece(0,anki.TrackPieceType.CURVE,True),
+        anki.TrackPiece(0,anki.TrackPieceType.CURVE,True),
+        anki.TrackPiece(0,anki.TrackPieceType.CURVE,True),
+        anki.TrackPiece(0,anki.TrackPieceType.STRAIGHT,True),
+        anki.TrackPiece(0,anki.TrackPieceType.CURVE,False),
+        anki.TrackPiece(0,anki.TrackPieceType.CURVE,False),
+        anki.TrackPiece(0,anki.TrackPieceType.CURVE,False),
+        anki.TrackPiece(0,anki.TrackPieceType.STRAIGHT,False),
+        anki.TrackPiece(0,anki.TrackPieceType.FINISH,False)]
     return map
 
 if __name__ == "__main__":
